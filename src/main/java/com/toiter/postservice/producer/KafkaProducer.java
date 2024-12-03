19ec885273d8b4dbe9b6c58700fa2370b22eeb7b
João Pedro Lopes <jl.moreno7@hotmail.com>
2024-12-02 23:12:59 -0400

    Ajustando KafkaProducer

---
+++ src/main/java/com/toiter/postservice/producer/KafkaProducer.java
@@ -0,0 +1,21 @@
+package com.toiter.postservice.producer;
+
+import com.toiter.postservice.model.PostCreatedEvent;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducer {
+    private final KafkaTemplate<String, PostCreatedEvent> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, PostCreatedEvent> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendPostCreatedEvent(PostCreatedEvent event) {
+        kafkaTemplate.executeInTransaction(operations -> {
+            operations.send("post-created-topic", event);
+            return true;
+        });
+    }
+}
