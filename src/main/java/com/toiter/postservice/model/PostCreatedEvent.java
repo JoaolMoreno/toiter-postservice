559f7c8e4f1bfaec487ed1a9e86f5f89045c989a
João Pedro Lopes Moreno <jl.moreno7@hotmail.com>
2024-12-03 13:50:36 -0400

    Ajustando classe adicionando construtor

--- src/main/java/com/toiter/postservice/model/PostCreatedEvent.java
+++ src/main/java/com/toiter/postservice/model/PostCreatedEvent.java
@@ -16,6 +16,9 @@ public class PostCreatedEvent {
         this.repostParentId = repostParentId;
     }
 
+    public PostCreatedEvent() {
+    }
+
     public Long getPostId() {
         return postId;
     }
