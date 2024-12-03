806daabaf84e94fc893895b611179300ac5dd288
João Pedro Lopes <jl.moreno7@hotmail.com>
2024-12-02 23:14:45 -0400

    Atualizando PostCreatedEvent

--- src/main/java/com/toiter/postservice/model/PostCreatedEvent.java
+++ src/main/java/com/toiter/postservice/model/PostCreatedEvent.java
@@ -6,12 +6,14 @@ public class PostCreatedEvent {
     private Long userId;
     private String content;
     private Long parentPostId;
+    private Long repostParentId;
 
-    public PostCreatedEvent(Long postId, Long userId, String content, Long parentPostId) {
+    public PostCreatedEvent(Long postId, Long userId, String content, Long parentPostId, Long repostParentId) {
         this.postId = postId;
         this.userId = userId;
         this.content = content;
         this.parentPostId = parentPostId;
+        this.repostParentId = repostParentId;
     }
 
     public Long getPostId() {
@@ -44,5 +46,13 @@ public class PostCreatedEvent {
 
     public void setParentPostId(Long parentPostId) {
         this.parentPostId = parentPostId;
+    }
+
+    public Long getRepostParentId() {
+        return repostParentId;
+    }
+
+    public void setRepostParentId(Long repostParentId) {
+        this.repostParentId = repostParentId;
     }
 }
