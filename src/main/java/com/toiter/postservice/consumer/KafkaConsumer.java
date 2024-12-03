639fa7310e65e765b46a055e79f081b61e9baec7
João Pedro Lopes Moreno <jl.moreno7@hotmail.com>
2024-12-03 18:43:31 -0400

    simplificando consumer

--- src/main/java/com/toiter/postservice/consumer/KafkaConsumer.java
+++ src/main/java/com/toiter/postservice/consumer/KafkaConsumer.java
@@ -1,7 +1,8 @@
 package com.toiter.postservice.consumer;
 
-import com.toiter.postservice.model.PostCreatedEvent;
 import com.toiter.postservice.model.PostData;
+import com.toiter.postservice.model.PostDeletedEvent;
+import com.toiter.postservice.model.PostEvent;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -19,22 +20,27 @@ public class KafkaConsumer {
         this.redisTemplateForPostData = redisTemplateForPostData;
     }
 
-    @KafkaListener(topics = "post-created-topic", groupId = "post-event-consumers")
-    private void processPostEvent(PostCreatedEvent event) {
-        PostData postData = new PostData(event);
+    @KafkaListener(topics = {"post-created-topic", "post-deleted-topic"}, groupId = "post-event-consumers")
+    private void processPostEvent(PostEvent event) {
+        PostData postData = new PostData(event.getPost());
+        if(event instanceof PostDeletedEvent) {
+            postData.setLikesCount(0);
+            postData.setRepostsCount(0);
+            postData.setViewCount(0);
+        }
 
-        // Salvar por ID do Post
+        // Save by Post ID
         String postPublicDataKey = POST_ID_DATA_KEY_PREFIX + postData.getId();
         redisTemplateForPostData.opsForValue().set(postPublicDataKey, postData, Duration.ofHours(1));
 
-        // Salvar por Parent ID
+        // Save by Parent ID
         if (postData.getParentPostId() != null) {
             String postParentDataKey = POST_PARENTID_DATA_KEY_PREFIX + postData.getParentPostId();
             redisTemplateForPostData.opsForList().rightPush(postParentDataKey, postData);
             redisTemplateForPostData.expire(postParentDataKey, Duration.ofHours(1));
         }
 
-        // Salvar por Repost ID
+        // Save by Repost ID
         if (postData.getRepostParentId() != null) {
             String postRepostDataKey = POST_REPOSTID_DATA_KEY_PREFIX + postData.getRepostParentId();
             redisTemplateForPostData.opsForList().rightPush(postRepostDataKey, postData);
