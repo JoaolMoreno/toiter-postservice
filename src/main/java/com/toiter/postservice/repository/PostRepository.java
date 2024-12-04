3241b8c6d661ae54d1dbec6ea66ff5986df8f808
João Pedro Lopes Moreno <jl.moreno7@hotmail.com>
2024-12-04 07:54:57 -0400

    Metodos para buscar Posts ja com as metricas carregadas

--- src/main/java/com/toiter/postservice/repository/PostRepository.java
+++ src/main/java/com/toiter/postservice/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package com.toiter.postservice.repository;
 
 import com.toiter.postservice.entity.Post;
+import com.toiter.postservice.model.PostData;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,6 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
@@ -17,4 +19,53 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
     @Query("SELECT p.id FROM Post p WHERE p.parentPostId = :parentPostId ORDER BY p.createdAt DESC limit 3")
     List<Long> findChildIdsByParentPostId(Long parentPostId, Pageable pageable);
+
+    @Query("""
+        SELECT new com.toiter.postservice.model.PostData(
+            p.id,
+            p.parentPostId,
+            p.repostParentId,
+            p.userId,
+            p.content,
+            p.mediaUrl,
+            COUNT(DISTINCT l.id) as likesCount,
+            COUNT(DISTINCT r.id) as repliesCount,
+            COUNT(DISTINCT rp.id) as repostsCount,
+            COUNT(DISTINCT v.id) as viewCount,
+            p.createdAt
+        )
+        FROM Post p
+        LEFT JOIN Like l ON l.post.id = p.id
+        LEFT JOIN Post r ON r.parentPostId = p.id
+        LEFT JOIN Post rp ON rp.repostParentId = p.id
+        LEFT JOIN View v ON v.post.id = p.id
+        WHERE p.id = :postId
+        GROUP BY p.id
+    """)
+    Optional<PostData> fetchPostData(Long postId);
+
+    @Query("""
+        SELECT new com.toiter.postservice.model.PostData(
+            p.id,
+            p.parentPostId,
+            p.repostParentId,
+            p.userId,
+            p.content,
+            p.mediaUrl,
+            COUNT(DISTINCT l.id) as likesCount,
+            COUNT(DISTINCT r.id) as repliesCount,
+            COUNT(DISTINCT rp.id) as repostsCount,
+            COUNT(DISTINCT v.id) as viewCount,
+            p.createdAt
+        )
+        FROM Post p
+        LEFT JOIN Like l ON l.post.id = p.id
+        LEFT JOIN Post r ON r.parentPostId = p.id
+        LEFT JOIN Post rp ON rp.repostParentId = p.id
+        LEFT JOIN View v ON v.post.id = p.id
+        WHERE p.parentPostId = :parentPostId
+        GROUP BY p.id
+    """)
+    Page<PostData> fetchChildPostsData(Long parentPostId, Pageable pageable);
+
 }
