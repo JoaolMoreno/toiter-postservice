2cce8cc72d92a27830304dc8cdd450fc60e8e68c
João Pedro Lopes <jl.moreno7@hotmail.com>
2025-01-05 01:14:28 -0400

    Criando endpoints /internal para uso entre serviços

---
+++ src/main/java/com/toiter/postservice/controller/InternalPostController.java
@@ -0,0 +1,27 @@
+package com.toiter.postservice.controller;
+
+import com.toiter.postservice.service.PostService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/internal/posts")
+public class InternalPostController {
+
+    private static final Logger logger = LoggerFactory.getLogger(InternalPostController.class);
+
+    private final PostService postService;
+
+    public InternalPostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping("/count")
+    public Integer getPostsCount(Long userId) {
+        logger.debug("Fetching posts count for user ID: {}", userId);
+        return postService.getPostsCount(userId);
+    }
+}
