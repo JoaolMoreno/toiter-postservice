295fd899666a755b111689c2a5d3c9710bb9e355
João Pedro Lopes Moreno <joao.moreno@bomfuturo.com.br>
2025-11-14 15:04:20 -0400

    Refatorando o serviço de cliente de usuário para remover dependências do Redis e simplificar a lógica de recuperação de dados

--- src/main/java/com/toiter/postservice/service/UserClientService.java
+++ src/main/java/com/toiter/postservice/service/UserClientService.java
@@ -4,16 +4,12 @@ import com.toiter.postservice.model.UserResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
-import java.time.Duration;
 
 @Service
 public class UserClientService {
@@ -26,86 +22,43 @@ public class UserClientService {
     @Value("${service.shared-key}")
     private String sharedKey;
 
-    private final RedisTemplate<String, Long> redisTemplateForLong;
-    private final RedisTemplate<String, String> redisTemplateForString;
-
-    private static final String USERNAME_TO_ID_KEY_PREFIX = "user:username:";
-
     private final Logger logger = LoggerFactory.getLogger(UserClientService.class);
 
-    public UserClientService(RestTemplate restTemplate, RedisTemplate<String, Long> redisTemplateForLong, RedisTemplate<String, String> redisTemplateForString) {
+    public UserClientService(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
-        this.redisTemplateForLong = redisTemplateForLong;
-        this.redisTemplateForString = redisTemplateForString;
     }
 
     public Long getUserIdByUsername(String username) {
         logger.debug("Fetching user ID for username: {}", username);
-        String userIdKey = USERNAME_TO_ID_KEY_PREFIX + username;
-        ValueOperations<String, Long> valueOpsForLong = redisTemplateForLong.opsForValue();
-        Number rawValue = valueOpsForLong.get(userIdKey);
-        Long userId = rawValue != null ? rawValue.longValue() : null;
+        String url = userServiceUrl + "/users/" + username + "/id";
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + sharedKey);
 
-        if (userId == null) {
-            logger.debug("User ID not found in cache, fetching from user service");
-            String url = userServiceUrl + "/users/" + username + "/id";
-            HttpHeaders headers = new HttpHeaders();
-            headers.set("Authorization", "Bearer " + sharedKey);
+        HttpEntity<Void> entity = new HttpEntity<>(headers);
+        ResponseEntity<Long> response = restTemplate.exchange(url, HttpMethod.GET, entity, Long.class);
+        Long userId = response.getBody();
 
-            HttpEntity<Void> entity = new HttpEntity<>(headers);
-            ResponseEntity<Long> response = restTemplate.exchange(url, HttpMethod.GET, entity, Long.class);
-            userId = response.getBody();
-
-            if(userId == null) {
-                throw new RuntimeException("User not found");
-            }
-
-            valueOpsForLong.set(userIdKey, userId);
+        if(userId == null) {
+            throw new RuntimeException("User not found");
         }
-        else {
-            logger.debug("User ID found in cache");
-        }
-        valueOpsForLong.getOperations().expire(userIdKey, Duration.ofHours(1));
 
         return userId;
     }
 
     public UserResponse getUserById(Long userId) {
         logger.debug("Fetching user for user ID: {}", userId);
-        String usernameKey = USERNAME_TO_ID_KEY_PREFIX + "id:" + userId;
-        String displayNameKey = USERNAME_TO_ID_KEY_PREFIX + "display:" + userId;
-        ValueOperations<String, String> valueOpsForString = redisTemplateForString.opsForValue();
-        String username = valueOpsForString.get(usernameKey);
-        String displayName = valueOpsForString.get(displayNameKey);
+        String url = userServiceUrl + "/users/" + userId + "/user";
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + sharedKey);
 
-        if (username == null || displayName == null) {
-            logger.debug("Username or display name not found in cache, fetching from user service");
-            String url = userServiceUrl + "/users/" + userId + "/user";
-            HttpHeaders headers = new HttpHeaders();
-            headers.set("Authorization", "Bearer " + sharedKey);
+        HttpEntity<Void> entity = new HttpEntity<>(headers);
+        ResponseEntity<UserResponse> response = restTemplate.exchange(url, HttpMethod.GET, entity, UserResponse.class);
+        UserResponse userResponse = response.getBody();
 
-            HttpEntity<Void> entity = new HttpEntity<>(headers);
-            ResponseEntity<UserResponse> response = restTemplate.exchange(url, HttpMethod.GET, entity, UserResponse.class);
-            UserResponse userResponse = response.getBody();
-
-            if (userResponse == null) {
-                throw new RuntimeException("User not found");
-            }
-
-            username = userResponse.getUsername();
-            displayName = userResponse.getDisplayName();
-
-            valueOpsForString.set(usernameKey, username);
-            valueOpsForString.set(displayNameKey, displayName);
-        } else {
-            logger.debug("Username and display name found in cache");
+        if (userResponse == null) {
+            throw new RuntimeException("User not found");
         }
-        valueOpsForString.getOperations().expire(usernameKey, Duration.ofHours(1));
-        valueOpsForString.getOperations().expire(displayNameKey, Duration.ofHours(1));
 
-        UserResponse userResponse = new UserResponse();
-        userResponse.setUsername(username);
-        userResponse.setDisplayName(displayName);
         return userResponse;
     }
 
