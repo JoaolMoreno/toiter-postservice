2ecff550efad4f271246cddbd49760fae6b43f5d
João Pedro Lopes Moreno <joao.moreno@bomfuturo.com.br>
2025-12-04 14:41:28 -0400

    Make main method public in ToiterPostserviceApplication

--- src/main/java/com/toiter/postservice/ToiterPostserviceApplication.java
+++ src/main/java/com/toiter/postservice/ToiterPostserviceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ToiterPostserviceApplication {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         Dotenv dotenv = Dotenv.configure()
                 .ignoreIfMissing()
                 .load();
